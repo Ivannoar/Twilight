22a0ed767e01d4ecb8439c00937f881d7bc35ce1
Ivan Li <li.ivan@outlook.com>
2022-04-27 02:51:58 -0400

    adding further script cleaning and additions/modifications to introduction/data/model sections of paper

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -13,7 +13,7 @@
 
 #### Workspace setup ####
 # Use R Projects, not setwd().
-install.packages("rtweet")
+
 library(haven)
 library(tidyverse)
 library(rtweet)
@@ -24,6 +24,7 @@ library(jsonlite)
 library(utils)
 library(httr)
 library(httpuv)
+library(lubridate)
 
 
 # Read in the raw data.
@@ -84,49 +85,51 @@ april22 <- search_tweets(q = "Wordle 307", n = 36000, retryonratelimit = TRUE, t
 april23 <- search_tweets(q = "Wordle 308", n = 36000, retryonratelimit = TRUE, type = "mixed")
   df <- apply(april23,2,as.character)
   write.csv(x=df, file = "C:/Users/liiva/OneDrive/april_23.csv")
+
+april26 <- search_tweets(q = "Wordle 310", n = 18000, type = "mixed")  
+
+april_4 <- read.csv('inputs/data/april_4.csv')
+april_5 <- read.csv('inputs/data/april_5.csv')
+april_6 <- read.csv('inputs/data/april_6.csv')
+april_7 <- read.csv('inputs/data/april_7.csv')
+april_8 <- read.csv('inputs/data/april_8.csv')
+april_9 <- read.csv('inputs/data/april_9.csv')
+april_10 <- read.csv('inputs/data/april_10.csv')
+april_17 <- read.csv('inputs/data/april_17.csv')
+april_18 <- read.csv('inputs/data/april_18.csv')
+april_19 <- read.csv('inputs/data/april_19.csv')
+april_20 <- read.csv('inputs/data/april_20.csv')
+april_21 <- read.csv('inputs/data/april_21.csv')
+april_22 <- read.csv('inputs/data/april_22.csv')
+april_23 <- read.csv('inputs/data/april_23.csv')
   
+april_4_clean <- filter(april_4, str_detect(text, 'Wordle 289 1/6|Wordle 289 2/6|Wordle 289 3/6|Wordle 289 4/6|Wordle 289 5/6|Wordle 289 6/6|Wordle 289 X/6'))
 
-april_4_clean <- filter(april_4, str_detect(text, 'Wordle 289 1/6|Wordle 289 2/6|Wordle 289 3/6|
-Wordle 289 4/6|Wordle 289 5/6|Wordle 289 6/6|Wordle 289 X/6'))
-
-april_5_clean <- filter(april_5, str_detect(text, 'Wordle 290 1/6|Wordle 290 2/6|Wordle 290 3/6|
-Wordle 290 4/6|Wordle 290 5/6|Wordle 290 6/6|Wordle 290 X/6'))
+april_5_clean <- filter(april_5, str_detect(text, 'Wordle 290 1/6|Wordle 290 2/6|Wordle 290 3/6|Wordle 290 4/6|Wordle 290 5/6|Wordle 290 6/6|Wordle 290 X/6'))
   
-april_6_clean <- filter(april_6, str_detect(text, 'Wordle 291 1/6|Wordle 291 2/6|Wordle 291 3/6|
-Wordle 291 4/6|Wordle 291 5/6|Wordle 291 6/6|Wordle 291 X/6'))
+april_6_clean <- filter(april_6, str_detect(text, 'Wordle 291 1/6|Wordle 291 2/6|Wordle 291 3/6|Wordle 291 4/6|Wordle 291 5/6|Wordle 291 6/6|Wordle 291 X/6'))
 
-april_7_clean <- filter(april_7, str_detect(text, 'Wordle 292 1/6|Wordle 292 2/6|Wordle 292 3/6|
-Wordle 292 4/6|Wordle 292 5/6|Wordle 292 6/6|Wordle 292 X/6'))
+april_7_clean <- filter(april_7, str_detect(text, 'Wordle 292 1/6|Wordle 292 2/6|Wordle 292 3/6|Wordle 292 4/6|Wordle 292 5/6|Wordle 292 6/6|Wordle 292 X/6'))
 
-april_8_clean <- filter(april_8, str_detect(text, 'Wordle 293 1/6|Wordle 293 2/6|Wordle 293 3/6|
-Wordle 293 4/6|Wordle 293 5/6|Wordle 293 6/6|Wordle 293 X/6'))
+april_8_clean <- filter(april_8, str_detect(text, 'Wordle 293 1/6|Wordle 293 2/6|Wordle 293 3/6|Wordle 293 4/6|Wordle 293 5/6|Wordle 293 6/6|Wordle 293 X/6'))
 
-april_9_clean <- filter(april_9, str_detect(text, 'Wordle 294 1/6|Wordle 294 2/6|Wordle 294 3/6|
-Wordle 294 4/6|Wordle 294 5/6|Wordle 294 6/6|Wordle 294 X/6'))
+april_9_clean <- filter(april_9, str_detect(text, 'Wordle 294 1/6|Wordle 294 2/6|Wordle 294 3/6|Wordle 294 4/6|Wordle 294 5/6|Wordle 294 6/6|Wordle 294 X/6'))
 
-april_10_clean <- filter(april_10, str_detect(text, 'Wordle 295 1/6|Wordle 295 2/6|Wordle 295 3/6|
-Wordle 295 4/6|Wordle 295 5/6|Wordle 295 6/6|Wordle 295 X/6'))
+april_10_clean <- filter(april_10, str_detect(text, 'Wordle 295 1/6|Wordle 295 2/6|Wordle 295 3/6|Wordle 295 4/6|Wordle 295 5/6|Wordle 295 6/6|Wordle 295 X/6'))
 
-april_17_clean <- filter(april_17, str_detect(text, 'Wordle 302 1/6|Wordle 302 2/6|Wordle 302 3/6|
-Wordle 302 4/6|Wordle 302 5/6|Wordle 302 6/6|Wordle 302 X/6'))
+april_17_clean <- filter(april_17, str_detect(text, 'Wordle 302 1/6|Wordle 302 2/6|Wordle 302 3/6|Wordle 302 4/6|Wordle 302 5/6|Wordle 302 6/6|Wordle 302 X/6'))
 
-april_18_clean <- filter(april_18, str_detect(text, 'Wordle 303 1/6|Wordle 303 2/6|Wordle 303 3/6|
-Wordle 303 4/6|Wordle 303 5/6|Wordle 303 6/6|Wordle 303 X/6'))
+april_18_clean <- filter(april_18, str_detect(text, 'Wordle 303 1/6|Wordle 303 2/6|Wordle 303 3/6|Wordle 303 4/6|Wordle 303 5/6|Wordle 303 6/6|Wordle 303 X/6'))
 
-april_19_clean <- filter(april_19, str_detect(text, 'Wordle 304 1/6|Wordle 304 2/6|Wordle 304 3/6|
-Wordle 304 4/6|Wordle 304 5/6|Wordle 304 6/6|Wordle 304 X/6'))
+april_19_clean <- filter(april_19, str_detect(text, 'Wordle 304 1/6|Wordle 304 2/6|Wordle 304 3/6|Wordle 304 4/6|Wordle 304 5/6|Wordle 304 6/6|Wordle 304 X/6'))
 
-april_20_clean <- filter(april_20, str_detect(text, 'Wordle 305 1/6|Wordle 305 2/6|Wordle 305 3/6|
-Wordle 305 4/6|Wordle 305 5/6|Wordle 305 6/6|Wordle 305 X/6'))
+april_20_clean <- filter(april_20, str_detect(text, 'Wordle 305 1/6|Wordle 305 2/6|Wordle 305 3/6|Wordle 305 4/6|Wordle 305 5/6|Wordle 305 6/6|Wordle 305 X/6'))
 
-april_21_clean <- filter(april_21, str_detect(text, 'Wordle 306 1/6|Wordle 306 2/6|Wordle 306 3/6|
-Wordle 306 4/6|Wordle 306 5/6|Wordle 306 6/6|Wordle 306 X/6'))
+april_21_clean <- filter(april_21, str_detect(text, 'Wordle 306 1/6|Wordle 306 2/6|Wordle 306 3/6|Wordle 306 4/6|Wordle 306 5/6|Wordle 306 6/6|Wordle 306 X/6'))
 
-april_22_clean <- filter(april_22, str_detect(text, 'Wordle 307 1/6|Wordle 307 2/6|Wordle 307 3/6|
-Wordle 307 4/6|Wordle 307 5/6|Wordle 307 6/6|Wordle 307 X/6'))
+april_22_clean <- filter(april_22, str_detect(text, 'Wordle 307 1/6|Wordle 307 2/6|Wordle 307 3/6|Wordle 307 4/6|Wordle 307 5/6|Wordle 307 6/6|Wordle 307 X/6'))
 
-april_23_clean <- filter(april_23, str_detect(text, 'Wordle 308 1/6|Wordle 308 2/6|Wordle 308 3/6|
-Wordle 308 4/6|Wordle 308 5/6|Wordle 308 6/6|Wordle 308 X/6'))
+april_23_clean <- filter(april_23, str_detect(text, 'Wordle 308 1/6|Wordle 308 2/6|Wordle 308 3/6|Wordle 308 4/6|Wordle 308 5/6|Wordle 308 6/6|Wordle 308 X/6'))
 
 cleanlist <- list(april_4_clean, april_5_clean, april_6_clean, april_7_clean, april_8_clean,
                   april_9_clean, april_10_clean, april_17_clean, april_18_clean, april_19_clean,
@@ -139,7 +142,7 @@ april_4_clean <- select(april_4_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -150,7 +153,7 @@ april_5_clean <- select(april_5_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -161,7 +164,7 @@ april_6_clean <- select(april_6_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -172,7 +175,7 @@ april_7_clean <- select(april_7_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -183,7 +186,7 @@ april_8_clean <- select(april_8_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -194,7 +197,7 @@ april_9_clean <- select(april_9_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -205,7 +208,7 @@ april_10_clean <- select(april_10_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -216,7 +219,7 @@ april_17_clean <- select(april_17_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -227,7 +230,7 @@ april_18_clean <- select(april_18_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -238,7 +241,7 @@ april_19_clean <- select(april_19_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -249,7 +252,7 @@ april_20_clean <- select(april_20_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -260,7 +263,7 @@ april_21_clean <- select(april_21_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -271,7 +274,7 @@ april_22_clean <- select(april_22_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
@@ -282,10 +285,45 @@ april_23_clean <- select(april_23_clean, c(text, created_at, place_name,
                  if_else(grepl('3/6', text), '3',
                  if_else(grepl('4/6', text), '4',
                  if_else(grepl('5/6', text), '5',
-                 if_else(grepl('6/6', text), '6', 'DNF'
+                 if_else(grepl('6/6', text), '6', '7'
                          )))))),
          hardmode = if_else(grepl('/6*', text, fixed = TRUE, useBytes = TRUE), TRUE, FALSE))
 
 masterdata <- rbind(april_4_clean, april_5_clean, april_6_clean, april_7_clean, april_8_clean,
                   april_9_clean, april_10_clean, april_17_clean, april_18_clean, april_19_clean,
                   april_20_clean, april_21_clean, april_22_clean, april_23_clean)
+masterdata <- masterdata |> 
+  mutate(score = str_replace(score, 'DNF', '7'))
+
+masterdata <- masterdata |>
+  mutate(time_tweeted = as_datetime(created_at))
+
+masterdata <- masterdata |>
+  mutate(time_tweeted_utc = format(time_tweeted,"%H:%M:%S"), date_tweeted = as.Date(time_tweeted))
+
+masterdata <- masterdata |>
+  mutate(time_tweeted_roundhour = format(round_date(time_tweeted, unit = 'hour'),"%H:%M:%S"))
+
+masterdata_clean <- masterdata |>
+  select(date_tweeted, country, country_code, score, hardmode)
+
+
+data_firstweek <- rbind(april_4_clean, april_5_clean, april_6_clean, april_7_clean, april_8_clean,
+                  april_9_clean, april_10_clean)
+data_secondweek <- rbind(april_17_clean, april_18_clean, april_19_clean,
+                  april_20_clean, april_21_clean, april_22_clean, april_23_clean)
+
+data_firstweek <- data_firstweek |> 
+  mutate(score = str_replace(score, 'DNF', '7'))
+data_secondweek <- data_secondweek |> 
+  mutate(score = str_replace(score, 'DNF', '7'))
+
+data_firstweek <- data_firstweek |>
+  mutate(time_tweeted = as_datetime(created_at))
+data_secondweek <- data_secondweek |>
+  mutate(time_tweeted = as_datetime(created_at))
+
+data_firstweek <- data_firstweek |>
+  mutate(time_tweeted_utc = format(time_tweeted,"%H:%M:%S"), date_tweeted = as.Date(time_tweeted))
+data_secondweek <- data_secondweek |>
+  mutate(time_tweeted_utc = format(time_tweeted,"%H:%M:%S"), date_tweeted = as.Date(time_tweeted))
